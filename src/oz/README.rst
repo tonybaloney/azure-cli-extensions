d6e08a4dffa30f2aa3c455747d4282e15c8d1810
Anthony Shaw <anthony.p.shaw@gmail.com>
2021-05-21 15:40:51 +1000

    add oz extension

---
+++ src/oz/README.rst
@@ -0,0 +1,5 @@
+Microsoft Azure CLI 'oz' Extension
+==========================================
+
+This package is for the 'oz' extension.
+i.e. 'az oz'
---
+++ src/oz/azext_oz/_help.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.help_files import helps  # pylint: disable=unused-import
+
+
+helps['oz'] = """
+    type: group
+    short-summary: Commands to manage Ozs.
+"""
+
+helps['oz create'] = """
+    type: command
+    short-summary: Create a Oz.
+"""
+
+helps['oz list'] = """
+    type: command
+    short-summary: List Ozs.
+"""
+
+# helps['oz delete'] = """
+#     type: command
+#     short-summary: Delete a Oz.
+# """
+
+# helps['oz show'] = """
+#     type: command
+#     short-summary: Show details of a Oz.
+# """
+
+# helps['oz update'] = """
+#     type: command
+#     short-summary: Update a Oz.
+# """
